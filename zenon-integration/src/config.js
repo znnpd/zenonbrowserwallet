778ad20f7a4ab193e2d3416fcfa2a78811c5cef9
peterfust <peter.fust@mailbox.org>
2022-01-06 21:35:09 +0100

    updated

---
+++ zenon-integration/src/config.js
@@ -0,0 +1,5 @@
+const nodeIp = '78.141.207.100';
+const httpPort = '35997';
+const wsPort = '35998';
+
+module.exports = {nodeIp, httpPort, wsPort};
--- zenon-integration/src/zenon.js
+++ zenon-integration/src/zenon.js
@@ -1,6 +1,8 @@
-const znn = require("../../../znn.js/src");
+const znn = require('../../../znn.js/src');
 const crypto = require('crypto-browserify');
 const bip39 = require('bip39');
+const rpc = require('./rpc');
+require('dotenv').config({ path: './.env' });
 
 async function createNewWallet(password) {
     var entropy = new Buffer.from(crypto.randomBytes(32), 'utf8');
@@ -9,9 +11,15 @@ async function createNewWallet(password) {
 }
 
 async function importExistingWallet(mnemonic, password) {
-    var entropy = bip39.mnemonicToEntropy(mnemonic)
+    var entropy = new Buffer.from(bip39.mnemonicToEntropy(mnemonic), 'hex');
     const wallet = JSON.stringify(await znn.wallet.KeyFile.Encrypt(entropy, password));
     localStorage.setItem('wallet', wallet);
 }
 
-module.exports = {createNewWallet, importExistingWallet};
+async function getBalance(address) {
+    rpc.sendRpcRequest('ledger.getAccountInfoByAddress', [address]).then((result) => {
+        return result;
+    });
+}
+
+module.exports = {createNewWallet, importExistingWallet, getBalance};
