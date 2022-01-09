aae41d64bf7768dd41f649744af9f83ed9626e78
peterfust <peter.fust@mailbox.org>
2022-01-09 18:21:03 +0100

    updated

--- zenon_integration/src/config.js
+++ zenon_integration/src/config.js
@@ -1,5 +1,3 @@
-const nodeIp = '78.141.207.100';
-const httpPort = '35997';
-const wsPort = '35998';
-
-module.exports = {nodeIp, httpPort, wsPort};
+export const nodeIp = '78.141.207.100';
+export const nodeHttpPort = '35997';
+export const netId = 1; // 1: mainnet
--- zenon_integration/src/zenon.js
+++ zenon_integration/src/zenon.js
@@ -1,25 +1,146 @@
 const znn = require('../../../znn.js/src');
+import {nodeIp, nodeHttpPort, netId} from './config';
 const crypto = require('crypto-browserify');
 const bip39 = require('bip39');
-const rpc = require('./rpc');
-require('dotenv').config({ path: './.env' });
+import { RequestManager, HTTPTransport, Client } from "@open-rpc/client-js";
+import * as ed from '@noble/ed25519';
+const {SHA3} = require("sha3");
+import { Buffer } from 'buffer';
 
-async function createNewWallet(password) {
+// RPC client
+const transport = new HTTPTransport('http://' + nodeIp + ':' + nodeHttpPort);
+const requestManager = new RequestManager([transport]);
+const client = new Client(requestManager);
+
+const znnTokenStandard = 'zts1znnxxxxxxxxxxxxx9z4ulx';
+const qsrTokenStandard = 'zts1qsrxxxxxxxxxxxxxmrhjll';
+const emptyTokenStandard = 'zts1qqqqqqqqqqqqqqqqtq587y';
+
+export async function createNewWallet(password) {
     var entropy = new Buffer.from(crypto.randomBytes(32), 'utf8');
     const wallet = JSON.stringify(await znn.wallet.KeyFile.Encrypt(entropy, password));
     localStorage.setItem('wallet', wallet);
 }
 
-async function importExistingWallet(mnemonic, password) {
+export async function importExistingWallet(mnemonic, password) {
     var entropy = new Buffer.from(bip39.mnemonicToEntropy(mnemonic), 'hex');
     const wallet = JSON.stringify(await znn.wallet.KeyFile.Encrypt(entropy, password));
     localStorage.setItem('wallet', wallet);
 }
 
-async function getBalance(address) {
-    rpc.sendRpcRequest('ledger.getAccountInfoByAddress', [address]).then((result) => {
-        return result;
-    });
+export function getAccountInfo(address) {
+    return client.request({method: 'ledger.getAccountInfoByAddress', params: [address]});
 }
 
-module.exports = {createNewWallet, importExistingWallet, getBalance};
+export async function sendTransaction(addressFrom, addressTo, amount, tokenStandard, password) {
+    let accountBlock = initAccountBlock();
+    accountBlock.address = addressFrom;
+    accountBlock.toAddress = addressTo;
+    accountBlock.amount = amount;
+    accountBlock.tokenStandard = tokenStandard;
+    
+    const frontierAccountBlock = await client.request({method: 'ledger.getFrontierAccountBlock', params: [addressFrom]});
+    if (frontierAccountBlock !== null) {
+        accountBlock.height = frontierAccountBlock.height + 1;
+        accountBlock.previousHash = frontierAccountBlock.hash;
+    }
+    
+    const frontierMomentum = await client.request({method: 'ledger.getFrontierMomentum', params: []});
+    const momentumAcknowledged = {
+        height: frontierMomentum.height,
+        hash: frontierMomentum.hash
+    };
+    accountBlock.momentumAcknowledged = momentumAcknowledged;
+    
+    const powParam = {
+        address: accountBlock.address,
+        blockType: accountBlock.blockType,
+        toAddress: accountBlock.toAddress,
+        data: ""
+    }
+    
+    var requiredPow = await client.request({method: 'embedded.plasma.getRequiredPoWForAccountBlock', params: [powParam]});
+    if (requiredPow.requiredDifficulty !== 0) {
+        console.error('ERROR: Transactions with PoW not supported yet!');
+        return;
+    } else {
+        accountBlock.fusedPlasma = requiredPow.basePlasma;
+    }
+    
+    var wallet = JSON.parse(localStorage.getItem('wallet'));
+    let entropy;
+    try {
+        entropy = await znn.wallet.KeyFile.Decrypt(wallet, password);
+    } catch (error) {
+        console.error(error)   
+    } 
+    
+    var transactionToHash = Buffer.concat([
+        longToBytes(accountBlock.version),
+        longToBytes(accountBlock.chainIdentifier),
+        longToBytes(accountBlock.blockType),
+        accountBlock.previousHash,
+        longToBytes(accountBlock.height),
+        accountBlock.momentumAcknowledged.hash,
+        longToBytes(accountBlock.momentumAcknowledged.height),
+        accountBlock.address,
+        accountBlock.toAddress,
+        bigIntToBytes(accountBlock.amount),
+        accountBlock.tokenStandard,
+        accountBlock.fromBlockHash,
+        SHA3(256).update(buffer.alloc(0)).digest(),
+        SHA3(256).update(accountBlock.data).digest(),
+        longToBytes(accountBlock.fusedPlasma),
+        longToBytes(accountBlock.difficulty),
+        this.nonce
+    ]);
+    
+    accountBlock.hash = SHA3(256).update(JSON.stringify(transactionToHash)).digest();
+    const keyPair = znn.wallet.KeyPair.FromEntropy(entropy);
+    const signature = await ed.sign(accountBlock.hash, keyPair.privateKey);
+    accountBlock.signature = signature.toString('hex');
+    accountBlock.publicKey = keyPair.publicKey.toString('base64');
+    console.log(accountBlock);
+    return client.request({method: 'ledger.publishRawTransaction', params: [accountBlock]});
+    
+}
+function longToBytes(value) {
+    let b = Buffer.alloc(8);
+    b.writeUInt32BE(value, 4);
+    return b;
+}
+
+function bigIntToBytes(value) {
+    let b = Buffer.alloc(32);
+    b.writeBigUInt64BE(BigInt(value), 24);
+    return b;
+}
+
+function initAccountBlock() {
+    const emptyHash = '0000000000000000000000000000000000000000000000000000000000000000';
+    const emptyHashHeight = {
+        height: 0,
+        hash: emptyHash
+    };
+    const emptyTokenStandard = 'zts1qqqqqqqqqqqqqqqqtq587y';
+    let accountBlock = {};
+    accountBlock.version = 1;
+    accountBlock.blockType = 2;  // 2: Send
+    accountBlock.fromBlockHash = emptyHash;
+    accountBlock.chainIdentifier = netId;
+    accountBlock.hash = emptyHash;
+    accountBlock.previousHash = emptyHash;
+    accountBlock.height = 0;
+    accountBlock.momentumAcknowledged = emptyHashHeight;
+    accountBlock.address = null;
+    accountBlock.toAddress = null;
+    accountBlock.amount = 0;
+    accountBlock.tokenStandard = emptyTokenStandard;
+    accountBlock.fusedPlasma = 0;
+    accountBlock.data = '';
+    accountBlock.difficulty = 0;
+    accountBlock.nonce = '0000000000000000';
+    accountBlock.publicKey = null;
+    accountBlock.signature = null;
+    return accountBlock;
+}
